7e85130f314c9cfbfe1684d6a1ab2c67ab58dbc7
HeshaniWickramasinghe <wickramasinghheshani@gmail.com>
2026-02-25 18:00:35 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Success");
+})
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log("Server is running on port", process.env.PORT);
+    })
+})
+.catch((err) => {
+    console.log(err);
+})
