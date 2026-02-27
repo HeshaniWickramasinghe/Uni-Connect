e169a807392a025831e6c07b02ccf70ab2c9c12f
ChenulRandiya10 <chenulrandiya10@gmail.com>
2026-02-27 18:02:43 +0530

    go
    go

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+console.log("Hello! This is the backend of Uni-Connect.");
+
+//mongo pass = chenulR123
+
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+// Middleware to parse JSON bodies
+app.use("/", (req, res, next) => {
+    res.send("It Is Working...");
+});
 
-app.get("/", (req, res) => {
-    res.send("Success");
-})
-
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect("mongodb+srv://chenul:chenulR123@uniconnect.wwb9cly.mongodb.net/")
 .then(() => console.log("Connected to MongoDB"))
 .then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log("Server is running on port", process.env.PORT);
-    })
+    app.listen(5000);
 })
-.catch((err) => {
-    console.log(err);
-})
+.catch((err) => console.log((err)));
