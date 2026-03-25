b2af6e4ed793bdd1de28da7e282757128f26ab1e
ChenulRandiya10 <chenulrandiya10@gmail.com>
2026-03-25 16:46:23 +0530

    delete
    delete

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
-const express  = require("express");
-const mongoose = require("mongoose");
+console.log("Hello! This is the backend of Uni-Connect.");
+
+const express = require('express');
+const mongoose = require('mongoose');
 require("dotenv").config();
 
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Success");
+// Middleware to parse JSON bodies
+app.use("/", (req, res, next) => {
+    res.send("It Is Working...");
 });
 
 mongoose.connect(process.env.MONGODB_URI)
@@ -15,6 +17,4 @@ mongoose.connect(process.env.MONGODB_URI)
         console.log("Server is running on port", process.env.PORT);
     });
 })
-.catch((err) => {
-    console.log(err);
-});
+.catch((err) => console.log((err)));
